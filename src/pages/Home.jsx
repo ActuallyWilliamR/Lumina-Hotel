c35a3e9c11af62dda7a34bd684ea12857577f554
Yepod <Williamrossander@hotmail.com>
2026-03-08 19:04:17 +0100

    Implemented Hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,41 @@
+import Hero from "../components/Hero"
+
 function Home() {
-  return <h1>Welcome to Lumina Hotel</h1>
+  return (
+    <>
+      <Hero />
+  
+      <section>
+        <h2>Travel Light</h2>
+        <p>
+          Lumina Hotel is designed for modern travelers and tech enthusiasts. 
+          Enjoy high-speed connectivity, smart rooms, and comfortable workspaces 
+          — the perfect environment for developers, creators, and remote professionals.
+        </p>
+      </section>
+
+      <section>
+        <h2>Our Rooms</h2>
+
+        <div>
+          <h3>Standard Room</h3>
+          <p>DESCRIPTION</p>
+        </div>
+
+        <div>
+          <h3>Deluxe Room</h3>
+          <p>DESCRIPTION</p>
+        </div>
+
+        <div>
+          <h3>Luminous Suite</h3>
+          <p>DESCRIPTIION</p>
+        </div>
+      </section>
+
+    </>
+  
+  )
 }
 
 export default Home
